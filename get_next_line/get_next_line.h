11b1a005f1e1a21a57b5b3dd3e356f03952f9217
Nour Smail <nsmail@f2r6s17.clusters.42paris.fr>
2025-05-16 17:56:55 +0200

    gnl_utils

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nsmail <nsmail@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/12 16:27:44 by nsmail            #+#    #+#             */
-/*   Updated: 2025/05/15 21:12:52 by nsmail           ###   ########.fr       */
+/*   Created: 2025/05/16 15:57:59 by nsmail            #+#    #+#             */
+/*   Updated: 2025/05/16 16:14:03 by nsmail           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdio.h>
-
 
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 10
 # endif
 
-#define BUFFER_MAX 1024
-
-char *get_next_line(int fd);
-char *clean_stash(char *stash);
-char *extract_line(char *stash);
-char *read_and_stash(int fd, char *stash);
-char *get_next_line(int fd);
-size_t ft_strlen(const char *str);
-char *ft_strchr(const char *s, int c);
-char *ft_strjoin(char *s1, const char *s2);
-char *ft_strdup(const char *s);
-char *ft_substr(const char *s, unsigned int start, size_t len);
-void ft_free(char *ptr);
+size_t  ft_strlen(const char *str);
+char    *ft_strchr(const char *s, int c);
 
 #endif
